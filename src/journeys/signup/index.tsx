ccbaea2d30d9214137ae9ab0325fc2a441a13e59
shruthik shetty <shrutikshetty14@gmail.com>
2025-05-18 19:45:35 +0530

    add modal

--- src/journeys/signup/index.tsx
+++ src/journeys/signup/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {View} from 'react-native';
 import HeaderLayout from '~/components/layoutes/HeaderLayout';
 import styles from './index.styles';
@@ -7,6 +7,8 @@ import FormLabelInput from '~/components/form-controllers/FormLabelInput';
 import {CustomerregisterSchemeType} from '~/navigation/signup/signup-schema';
 import {useFormContext} from 'react-hook-form';
 import Button from '~/components/buttons/Button';
+import ConfirmPhoneModal from './components/ConfirmPhoneModal';
+import {useModal} from '~/common/hooks/use-model';
 
 //** this is the sign up langing page where we start with verifying user phone number  */
 const SignUpLanding = () => {
@@ -15,8 +17,8 @@ const SignUpLanding = () => {
   const phoneNumber = watch('phoneNumber');
   // holdes the data if phone number is valied or not
   const isPhoneValid = !!phoneNumber && !formState.errors.phoneNumber;
-
-  console.log(phoneNumber);
+  // used to handle the confirm phone numbe modal
+  const model = useModal();
   return (
     <HeaderLayout
       progressPercent={12}
@@ -42,9 +44,18 @@ const SignUpLanding = () => {
       <Button
         disabled={!isPhoneValid}
         theme="Primary"
-        handlePress={() => {}}
+        handlePress={() => {
+          model.show(); // show the modal
+        }}
         label="Send otp"
         customStyles={{root: styles.button}}
+      />
+      <ConfirmPhoneModal
+        modal={model}
+        phoneNumber={phoneNumber}
+        handleConfim={() => {
+          console.log('calling otp');
+        }}
       />
     </HeaderLayout>
   );
