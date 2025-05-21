4ab6c23b76cbe2877b0c85efdaf2dd4e2a801dcc
shruthik shetty <shrutikshetty14@gmail.com>
2025-05-21 11:40:59 +0530

    add svgs

--- src/journeys/signup/add-home-address/index.tsx
+++ src/journeys/signup/add-home-address/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import HeaderLayout from '~/components/layouts/HeaderLayout';
 import TitleSubtitle from '~/components/text-display/TitleSubtitle';
 
+// This is the form screen for filling in the home address
 const AddHomeAddress = () => {
   return (
     <HeaderLayout progressPercent={28}>
--- src/journeys/signup/add-personal-info/index.tsx
+++ src/journeys/signup/add-personal-info/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import styles from './index.styles';
+import TitleSubtitle from '~/components/text-display/TitleSubtitle';
+import HeaderLayout from '~/components/layouts/HeaderLayout';
 
 const AddPersonalInfo = () => {
   return (
-    <View style={styles.container}>
-      <Text>AddPersonalInfo Component</Text>
-    </View>
+    <HeaderLayout progressPercent={28}>
+      <TitleSubtitle
+        title={'Personal Info'}
+        subTitle="This info need's to be accurate with your ID document ."
+      />
+    </HeaderLayout>
   );
 };
 
